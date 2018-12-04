dae04929c6906133e2a0a857b822d8570d39ece5
Dmitry Kryukov <remasik@gmail.com>
2018-12-04 13:16:14 -0500

    Attempt to translate contract to the syntax of version 1.4.1

--- volentixfrst/volentixfrst.cpp
+++ volentixfrst/volentixfrst.cpp
@@ -1,88 +1,98 @@
 #include <eosiolib/eosio.hpp>
 #include <eosiolib/multi_index.hpp>
-#include <eosiolib/types.hpp>
 #include <string>
 #include <eosiolib/transaction.hpp>
-#include<eosiolib/time.hpp>
+#include <eosiolib/time.hpp>
 
 using namespace eosio;
+using namespace std;
 
-class Ledger: public contract {
-	public:
-		Ledger(account_name s) :
-				contract(s), ledger(s, s) {
-		}
-		using contract::contract;[[eosio::action]]
-		void rcrdtfr(account_name s, std::string fromaccount,
-				std::string toaccount, double amount, std::string tokey, std::string comment, std::string nonce) {
+CONTRACT Ledger: public contract {
+  public:
+    using contract::contract;
 
-			double negAmount = -1 * amount;
-			double posAmount = amount;
+    /*
+    For the applying actions we need:
+    receiver - is the account that is currently processing the action.
+    code     - is the account that authorized the contract.
+    action   - is the ID of the currently running action.
+    */
+    Ledger(name receiver, name code,  datastream<const char*> ds): contract(receiver, code, ds), ledger(receiver, receiver.value) {}
 
-			uint64_t tbn = tapos_block_num();
-			uint64_t tbp = tapos_block_prefix();
-			uint64_t timestamp = current_time();
+    ACTION rcrdtfr(name signer,
+                  string fromaccount,
+                  string toaccount,
+                  double amount,
+                  string tokey,
+                  string comment,
+                  string nonce) {
 
-			ledger.emplace(get_self(), [&](auto& p)
-			{
-				p.key = ledger.available_primary_key();
-				p.Id = ledger.available_primary_key();
-				p.fromAccount = fromaccount;
-				p.toAccount = "";
-				p.sToKey = "";
-				p.amount = negAmount;
-				p.comment = comment;
-				p.nonce = nonce;
-				p.tbn = tbn;
-				p.tbp = tbp;
-				p.timestamp = timestamp;
-			});
-			//increase with tokey
-			ledger.emplace(get_self(), [&](auto& p)
-			{
-				p.key = ledger.available_primary_key();
-				p.Id = ledger.available_primary_key();
-				p.fromAccount = "";
-				p.toAccount = toaccount;
-				p.sToKey = tokey;
-				p.amount = posAmount;
-				p.comment = comment;
-				p.nonce = nonce;
-				p.tbn = tbn;
-				p.tbp = tbp;
-				p.timestamp = timestamp;
-			});
+      // ledgertable ledger(_code, _code.value);
 
-		}
+      double negAmount = -1 * amount;
+      double posAmount = amount;
 
-	private:
+      uint64_t tbn = tapos_block_num();
+      uint64_t tbp = tapos_block_prefix();
+      uint64_t timestamp = current_time();
 
-		struct [[eosio::table]] entry {
-				account_name s;
-				uint64_t key = 0;
-				uint64_t Id = 0;
-				std::string sToKey;
-				std::string fromAccount;
-				std::string toAccount;
-				double amount;
-				std::string comment;
-				std::string nonce;
-				int64_t tbn;
-				int64_t tbp;
-				uint64_t timestamp;
-				uint64_t primary_key() const {
-					return key;
-				}
-				uint64_t by_Id() const {
-					return Id;
-				}
+      ledger.emplace(get_self(), [&](auto& p)
+      {
+        p.key = ledger.available_primary_key();
+        p.Id = ledger.available_primary_key();
+        p.fromAccount = fromaccount;
+        p.toAccount = "";
+        p.sToKey = "";
+        p.amount = negAmount;
+        p.comment = comment;
+        p.nonce = nonce;
+        p.tbn = tbn;
+        p.tbp = tbp;
+        p.timestamp = timestamp;
+      });
+      //increase with tokey
+      ledger.emplace(get_self(), [&](auto& p)
+      {
+        p.key = ledger.available_primary_key();
+        p.Id = ledger.available_primary_key();
+        p.fromAccount = "";
+        p.toAccount = toaccount;
+        p.sToKey = tokey;
+        p.amount = posAmount;
+        p.comment = comment;
+        p.nonce = nonce;
+        p.tbn = tbn;
+        p.tbp = tbp;
+        p.timestamp = timestamp;
+      });
 
-		};
-		typedef eosio::multi_index<N(entry), entry,
-				indexed_by<N(Id), const_mem_fun<entry, uint64_t, &entry::by_Id>>> ledgertable;
-		ledgertable ledger;
+    }
 
+  private:
 
+    TABLE entry {
+        name signer;
+        uint64_t key = 0;
+        uint64_t Id = 0;
+        string sToKey;
+        string fromAccount;
+        string toAccount;
+        double amount;
+        string comment;
+        string nonce;
+        int64_t tbn;
+        int64_t tbp;
+        uint64_t timestamp;
+        uint64_t primary_key() const { return key; }
+        uint64_t by_Id() const { return Id; }
+
+    };
+
+    typedef multi_index<"entry"_n, entry, indexed_by<"Id"_n, const_mem_fun<entry, uint64_t, &entry::by_Id>>> ledger_table;
+
+    using set_action = action_wrapper<"rcrdtfr"_n, &Ledger::rcrdtfr>;
+
+    ledger_table ledger;
 };
 
-EOSIO_ABI( Ledger,(rcrdtfr))
+EOSIO_DISPATCH(Ledger,(rcrdtfr))
