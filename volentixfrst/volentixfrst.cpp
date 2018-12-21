aaede3fd5817851eba42c4104f9e7fd3f0f349f4
Dmitry Kryukov <remasik@gmail.com>
2018-12-21 11:51:53 -0500

    Rewrite contract to the new syntax

--- volentixfrst/volentixfrst.cpp
+++ volentixfrst/volentixfrst.cpp
@@ -8,91 +8,89 @@ using namespace eosio;
 using namespace std;
 
 CONTRACT Ledger: public contract {
-  public:
-    using contract::contract;
+	public:
+		using contract::contract;
 
-    /*
-    For the applying actions we need:
-    receiver - is the account that is currently processing the action.
-    code     - is the account that authorized the contract.
-    action   - is the ID of the currently running action.
-    */
-    Ledger(name receiver, name code,  datastream<const char*> ds): contract(receiver, code, ds), ledger(receiver, receiver.value) {}
+		Ledger(name self, name code, datastream<const char*> ds):contract(self, code, ds) {}
+		// The constructor below throw an error
+		// Ledger(name s, name code,  datastream<const char*> ds): contract(s, code, ds), ledger(s, s.value) {}
 
-    ACTION rcrdtfr(name signer,
-                  string fromaccount,
-                  string toaccount,
-                  double amount,
-                  string tokey,
-                  string comment,
-                  string nonce) {
 
-      // ledgertable ledger(_code, _code.value);
+		ACTION rcrdtfr(const name self,
+									string fromaccount,
+									string toaccount,
+									double amount,
+									string tokey,
+									string comment,
+									string nonce) {
 
-      double negAmount = -1 * amount;
-      double posAmount = amount;
+			double negAmount = -1 * amount;
+			double posAmount = amount;
 
-      uint64_t tbn = tapos_block_num();
-      uint64_t tbp = tapos_block_prefix();
-      uint64_t timestamp = current_time();
+			uint64_t tbn = tapos_block_num();
+			uint64_t tbp = tapos_block_prefix();
+			uint64_t timestamp = current_time();
 
-      ledger.emplace(get_self(), [&](auto& p)
-      {
-        p.key = ledger.available_primary_key();
-        p.Id = ledger.available_primary_key();
-        p.fromAccount = fromaccount;
-        p.toAccount = "";
-        p.sToKey = "";
-        p.amount = negAmount;
-        p.comment = comment;
-        p.nonce = nonce;
-        p.tbn = tbn;
-        p.tbp = tbp;
-        p.timestamp = timestamp;
-      });
-      //increase with tokey
-      ledger.emplace(get_self(), [&](auto& p)
-      {
-        p.key = ledger.available_primary_key();
-        p.Id = ledger.available_primary_key();
-        p.fromAccount = "";
-        p.toAccount = toaccount;
-        p.sToKey = tokey;
-        p.amount = posAmount;
-        p.comment = comment;
-        p.nonce = nonce;
-        p.tbn = tbn;
-        p.tbp = tbp;
-        p.timestamp = timestamp;
-      });
+			ledgertable ledger(_self, _self);
 
-    }
+			ledger.emplace(get_self(), [&](auto& p)
+			{
+				p.key = ledger.available_primary_key() + 1;
+				p.Id = ledger.available_primary_key() + 1;
+				p.fromAccount = fromaccount;
+				p.toAccount = "";
+				p.sToKey = "";
+				p.amount = negAmount;
+				p.comment = comment;
+				p.nonce = nonce;
+				p.tbn = tbn;
+				p.tbp = tbp;
+				p.timestamp = timestamp;
+			});
+			//increase with tokey
+			ledger.emplace(get_self(), [&](auto& p)
+			{
+				p.key = ledger.available_primary_key() + 1;
+				p.Id = ledger.available_primary_key() + 1;
+				p.fromAccount = "";
+				p.toAccount = toaccount;
+				p.sToKey = tokey;
+				p.amount = posAmount;
+				p.comment = comment;
+				p.nonce = nonce;
+				p.tbn = tbn;
+				p.tbp = tbp;
+				p.timestamp = timestamp;
+			});
+		}
 
-  private:
+	private:
 
-    TABLE entry {
-        name signer;
-        uint64_t key = 0;
-        uint64_t Id = 0;
-        string sToKey;
-        string fromAccount;
-        string toAccount;
-        double amount;
-        string comment;
-        string nonce;
-        int64_t tbn;
-        int64_t tbp;
-        uint64_t timestamp;
-        uint64_t primary_key() const { return key; }
-        uint64_t by_Id() const { return Id; }
+		TABLE entry {
+				name self;
+				uint64_t key = 0;
+				uint64_t Id = 0;
+				string sToKey;
+				string fromAccount;
+				string toAccount;
+				double amount;
+				string comment;
+				string nonce;
+				int64_t tbn;
+				int64_t tbp;
+				uint64_t timestamp;
+				uint64_t primary_key() const { return key; }
+				uint64_t by_Id() const { return Id; }
 
-    };
+				// EOSLIB_SERIALIZE( rcrdtfr, (s)(key)(Id)(sToKey)(fromAccount)(toAccount)(amount)(comment)(nonce) )
+		};
 
-    typedef multi_index<"entry"_n, entry, indexed_by<"Id"_n, const_mem_fun<entry, uint64_t, &entry::by_Id>>> ledger_table;
+		typedef multi_index<"entry"_n, entry, indexed_by<"Id"_n, const_mem_fun<entry, uint64_t, &entry::by_Id>>> ledgertable;
 
-    using set_action = action_wrapper<"rcrdtfr"_n, &Ledger::rcrdtfr>;
+		using set_action = action_wrapper<"rcrdtfr"_n, &Ledger::rcrdtfr>;
 
-    ledger_table ledger;
+		ledgertable ledger;
+
 };
 
-EOSIO_DISPATCH(Ledger,(rcrdtfr))
+EOSIO_DISPATCH( Ledger,(rcrdtfr))
